960c2ec767301ca2331544419eeb32a70e0ad05f
Martín <martineduardomorales@gmail.com>
2017-01-30 18:54:20 -0300

    cambio prueba

--- js/auth.js
+++ js/auth.js
@@ -3,28 +3,32 @@ var t = TrelloPowerUp.iframe();
 
 
 
-//$.ajax({
-//	url:"https://martinm78.github.io/ajax.html?isTokenOk="+token,
-//	dataType: 'json', 
-//	success:function(json){
-//		// do stuff with json (in this case an array)
-//		//alert("Success");
-//		console.log("Success");
-//		//return t.closePopup();
-//		//return t.closePopup();
-//		t.overlay({
-//			url: './overlay.html',
-//			args: { rand: (Math.random() * 100).toFixed(0) }
-//		  })
-//		  .then(function(){
-//			return t.closePopup();
-//		  });
-//	},
-//	error:function(jqXHR,textStatus){
-//		//alert("Error:"+textStatus);
-//		console.log("Error:"+textStatus);
-//	}      
-//});
+t.get('board', 'private', 'token')
+	.then(function(token){
+		
+		if (token) {
+			$.ajax({
+				url:"https://martinm78.github.io/ajax2.html?isTokenOk="+token,
+				dataType: 'json', 
+				success:function(json){
+					if (0 == json.status) {
+						// do stuff with json (in this case an array)
+						console.log("Success0");
+						t.overlay({
+							url: './overlay.html',
+							args: { rand: (Math.random() * 100).toFixed(0) }
+						  })
+						  .then(function(){
+							return t.closePopup();
+						  });
+					}
+				},
+				error:function(jqXHR,textStatus){
+					console.log("Error0:"+textStatus);
+				}      
+			});
+		} 
+});
 
 
 
@@ -42,49 +46,43 @@ var authBtn = document.getElementById('authorize');
 
 authBtn.addEventListener('click', function() {
 	
-  t.authorize(oauthUrl, authorizeOpts)
-  .then(function(token) {
-	console.log('then token:' + token);
-    return t.set('board', 'private', 'token', token)
-    .catch(t.NotHandled, function() {
-      // fall back to storing at board level
-		console.log('Error. Fall back to storing at organization level');
-      //return t.set('board', 'private', 'token', token);
-    });
-  })
-  .then(function() {
-	console.log('then closePopup');
-    // now that the token is stored, we can close this popup
-    // you might alternatively choose to open a new popup
-	t.get('board', 'private', 'token')
-		.then(function(token){
-			$.ajax({
-				url:"https://martinm78.github.io/ajax.html?token="+token,
-				dataType: 'json', 
-				success:function(json){
-					// do stuff with json (in this case an array)
-					console.log("Success");
-					t.overlay({
-						url: './overlay.html',
-						args: { rand: (Math.random() * 100).toFixed(0) }
-					  })
-					  .then(function(){
-						return t.closePopup();
-					  });
-				},
-				error:function(jqXHR,textStatus){
-					console.log("Error:"+textStatus);
-				}      
-			});
+	t.authorize(oauthUrl, authorizeOpts)
+	.then(function(token) {
+	  console.log('then token:' + token);
+	  return t.set('board', 'private', 'token', token)
+	  .catch(t.NotHandled, function() {
+		// fall back to storing at board level
+		  console.log('Error. Fall back to storing at organization level');
+		//return t.set('board', 'private', 'token', token);
+	  });
+	})
+	.then(function() {
+		console.log('then closePopup');
+		// now that the token is stored, we can close this popup
+		// you might alternatively choose to open a new popup
+		t.get('board', 'private', 'token')
+			.then(function(token){
+				$.ajax({
+					url:"https://martinm78.github.io/ajax.html?token="+token,
+					dataType: 'json', 
+					success:function(json){
+						// do stuff with json (in this case an array)
+						console.log("Success");
+						t.overlay({
+							url: './overlay.html',
+							args: { rand: (Math.random() * 100).toFixed(0) }
+						  })
+						  .then(function(){
+							return t.closePopup();
+						  });
+					},
+					error:function(jqXHR,textStatus){
+						console.log("Error:"+textStatus);
+					}      
+				});
+		});
+
 	});
-	
-	
-		
-		
-	
-    
-	  
-	
-  });
+
 });
 
