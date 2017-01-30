59d6b86a035c728873cb5a2848cbd5ad58bc0906
Martín <martineduardomorales@gmail.com>
2017-01-30 18:21:32 -0300

    cambio prueba

--- js/auth.js
+++ js/auth.js
@@ -1,24 +1,11 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
+var oauthUrl = 'https://martinm78.github.io/loginFromTrello.html';
 
-//var oauthUrl = 'https://trello.com/1/authorize?expiration=never' +
-//  '&name=[APPNAME]&scope=read&key=[APIKEY]&callback_method=fragment' +
-//  '&return_url=[RETURNURL]';
-  
-  var oauthUrl = 'https://martinm78.github.io/loginFromTrello.html';
-  
-  
-  
-
-var tokenLooksValid = function(token) {
-  //return /^[0-9a-f]{64}$/.test(token);
-  return true;
-}
 
 var authorizeOpts = {
   height: 680,
-  width: 580,
-  validToken: tokenLooksValid
+  width: 580
 };
 
 var authBtn = document.getElementById('authorize');
